29b5fc4f7eb09dd935537b49ef6f30aa8e686e50
Jasper Wallace <jasper@pointless.net>
2011-01-17 01:11:05 +0000

    add more test targets

--- test.sh
+++ test.sh
@@ -5,3 +5,7 @@ rm -f /tmp/*.t
 for f in ONE.MOL TWO.MOL THREE.MOL FOUR.MOL ; do
 ./fiddle.py test-files/${f} > /tmp/`basename $f .MOL`.t
 done
+
+for f in 1X1CM.MOL 2X2CM.MOL 4X4CM.MOL ; do
+./fiddle.py test-files/squares_mol_files/${f} > /tmp/`basename $f .MOL`.t
+done
